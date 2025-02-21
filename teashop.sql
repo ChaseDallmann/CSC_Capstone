985427d91173fbffad4867b19358faf569ea99b1
Chase Dallmann <chasedallmann@comcast.net>
2025-02-21 11:09:07 -0600

    teashop SQL Chase
    Signed-off-by: Chase Dallmann <chasedallmann@comcast.net>

--- teashop.sql
+++ teashop.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Feb 18, 2025 at 01:25 AM
+-- Generation Time: Feb 21, 2025 at 04:42 PM
 -- Server version: 8.0.39
--- PHP Version: 8.2.25
+-- PHP Version: 8.2.23
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -28,22 +28,23 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `category` (
-  `category_id` int NOT NULL,
-  `categoryName` varchar(40) COLLATE utf8mb4_general_ci NOT NULL,
-  `categoryDescription` varchar(200) COLLATE utf8mb4_general_ci NOT NULL
+  `categoryID` int NOT NULL,
+  `categoryName` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `categoryDescription` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `category`
 --
 
-INSERT INTO `category` (`category_id`, `categoryName`, `categoryDescription`) VALUES
+INSERT INTO `category` (`categoryID`, `categoryName`, `categoryDescription`) VALUES
 (1, 'Black Tea', 'A dark, bold type of tea. It dries for a long time to give it a dark look and potent aroma. Potential benefits include decreased risk of heart attack, blood pressure, and lower cholesterol.'),
 (2, 'Green Tea', 'Green tea is a lighter tea that has a more delicate somewhat grassy flavor. Potential benefits include improved alertness, less headaches, and weight loss.'),
 (3, 'White Tea', 'White tea has a sweet aroma with a soft flavor. Potential health benefits include reduced cholesterol levels, lower stress, healthy skin, and an increased metabolism.'),
 (4, 'Oolong Tea', 'Oolong is similar to green tea with a lighter oxidation process. Potential health benefits include reduced chronic diseases, increased metabolism, and antioxidants.'),
-(5, 'Herbal Tea', 'Herbal tea is made of herbs, fruit, and spices. There are various health benefits of herbal tea that are dependent on the ingredients used.'),
-(6, 'Pu-erh Tea', 'Pu-erh tea comes from the Yunnan province in China and has a rich, earthy flavor. Potential health benefits include an increased metabolism, anti-cancer properties, and boosted liver function.');
+(5, 'Yellow Tea', 'Yellow tea is rare and has a mild taste similar to green and white tea. Potential health benefits include anti-inflammatory properties, antioxidants, and anti-cancer characteristics.'),
+(6, 'Pu-erh Tea', 'Pu-erh tea comes from the Yunnan province in China and has a rich, earthy flavor. Potential health benefits include an increased metabolism, anti-cancer properties, and boosted liver function.'),
+(11, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -52,23 +53,28 @@ INSERT INTO `category` (`category_id`, `categoryName`, `categoryDescription`) VA
 --
 
 CREATE TABLE `customer` (
-  `customer_id` int NOT NULL,
-  `firstName` varchar(20) COLLATE utf8mb4_general_ci NOT NULL,
-  `lastName` varchar(30) COLLATE utf8mb4_general_ci NOT NULL,
-  `email` varchar(60) COLLATE utf8mb4_general_ci NOT NULL,
-  `streetAddress` varchar(30) COLLATE utf8mb4_general_ci NOT NULL,
-  `city` varchar(30) COLLATE utf8mb4_general_ci NOT NULL,
-  `state` varchar(20) COLLATE utf8mb4_general_ci NOT NULL,
+  `customerID` int NOT NULL,
+  `firstName` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `lastName` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `email` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `streetAddress` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `city` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `state` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
   `zipcode` int NOT NULL,
-  `password` varchar(20) COLLATE utf8mb4_general_ci NOT NULL
+  `password` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `customer`
 --
 
-INSERT INTO `customer` (`customer_id`, `firstName`, `lastName`, `email`, `streetAddress`, `city`, `state`, `zipcode`, `password`) VALUES
-(1, 'Natasha', 'Czaplewski', 'czaplewn@csp.edu', '123 Main Street', 'Winona', 'Minnesota', 55987, 'example1');
+INSERT INTO `customer` (`customerID`, `firstName`, `lastName`, `email`, `streetAddress`, `city`, `state`, `zipcode`, `password`) VALUES
+(1, 'Natasha', 'Czaplewski', 'czaplewn@csp.edu', '123 Main Street', 'Winona', 'Minnesota', 55987, 'example1'),
+(11, 'FRYPD', 'RKROL', 'VI938@hotmail.com', '732 QQRX', 'VMMVF', 'NFE', 28785, 'QDXBU395'),
+(13, 'REOPK', 'OAUVT', 'WZ567@hotmail.com', '163 TWNS', 'QCNKR', 'BYZ', 93872, 'KRZTM782'),
+(15, 'NZMEM', 'HZXMY', 'ZN289@yahoo.com', '496 LWJP', 'NNYSF', 'RUP', 67386, 'IFOVZ038'),
+(17, 'UPLSN', 'LTRGA', 'ZQ929@hotmail.com', '256 DNIQ', 'GFHGB', 'RCB', 70480, 'DMWBP584'),
+(19, 'IOGKY', 'XEIDG', 'WX881@aol.com', '511 EKNW', 'EHCFY', 'SXO', 90761, 'TEWKS239');
 
 -- --------------------------------------------------------
 
@@ -77,27 +83,25 @@ INSERT INTO `customer` (`customer_id`, `firstName`, `lastName`, `email`, `street
 --
 
 CREATE TABLE `manufacturer` (
-  `manufacturer_id` int NOT NULL,
-  `manufacturerName` varchar(40) COLLATE utf8mb4_general_ci NOT NULL,
-  `manufacturerURL` varchar(150) COLLATE utf8mb4_general_ci NOT NULL
+  `manufacturerID` int NOT NULL,
+  `manufacturerName` varchar(40) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `manufacturerURL` varchar(150) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `manufacturer`
 --
 
-INSERT INTO `manufacturer` (`manufacturer_id`, `manufacturerName`, `manufacturerURL`) VALUES
-(1, 'Full Leaf Tea Co.', 'https://fullleafteacompany.com/'),
-(2, 'San Francisco Herb Co.', 'https://www.sfherb.com/'),
-(3, 'Harney & Sons', 'https://www.harney.com/');
+INSERT INTO `manufacturer` (`manufacturerID`, `manufacturerName`, `manufacturerURL`) VALUES
+(1, 'DOPMO', '12345@test.com');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `order`
+-- Table structure for table `orders`
 --
 
-CREATE TABLE `order` (
+CREATE TABLE `orders` (
   `order_id` int NOT NULL,
   `customer_id` int NOT NULL COMMENT 'fk',
   `product_id` int NOT NULL COMMENT 'fk',
@@ -112,28 +116,21 @@ CREATE TABLE `order` (
 --
 
 CREATE TABLE `product` (
-  `product_id` int NOT NULL,
-  `productDescription` varchar(40) COLLATE utf8mb4_general_ci NOT NULL,
+  `productID` int NOT NULL,
+  `productName` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `productDescription` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
   `price` float(5,2) NOT NULL,
   `productInventory` int NOT NULL,
-  `category_id` int NOT NULL COMMENT 'fk',
-  `manufacturer_id` int NOT NULL COMMENT 'fk'
+  `categoryid` int NOT NULL,
+  `manufacturerid` int NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `product`
 --
 
-INSERT INTO `product` (`product_id`, `productDescription`, `price`, `productInventory`, `category_id`, `manufacturer_id`) VALUES
-(1, 'Organic Loose Leaf Tea, 1 OZ', 10.99, 10, 2, 1),
-(2, 'Organic Turmeric Loose Leaf 2 OZ', 11.99, 10, 2, 1),
-(3, 'Organic Earl Grey Loose Leaf 2 OZ', 14.99, 10, 1, 1),
-(4, 'Vanilla Loose Leaf 2 OZ', 12.99, 10, 1, 1),
-(5, 'Organic Oolong Loose Leaf 2 OZ', 16.99, 10, 4, 1),
-(6, 'Organic Royal Oolong Loose Leaf 2 OZ', 14.99, 10, 4, 1),
-(7, 'Mutan White Loose Leaf 1.5 OZ ', 11.00, 10, 3, 3),
-(8, 'Chamomile Tea Bags 8 OZ', 7.99, 10, 5, 2),
-(9, 'Pu-erh Loose Leaf 8 OZ', 12.99, 10, 6, 2);
+INSERT INTO `product` (`productID`, `productName`, `productDescription`, `price`, `productInventory`, `categoryid`, `manufacturerid`) VALUES
+(1, 'Boba Tea', 'A delicious boba tea', 10.99, 0, 11, 1);
 
 --
 -- Indexes for dumped tables
@@ -143,31 +140,87 @@ INSERT INTO `product` (`product_id`, `productDescription`, `price`, `productInve
 -- Indexes for table `category`
 --
 ALTER TABLE `category`
-  ADD PRIMARY KEY (`category_id`);
+  ADD PRIMARY KEY (`categoryID`);
 
 --
 -- Indexes for table `customer`
 --
 ALTER TABLE `customer`
-  ADD PRIMARY KEY (`customer_id`);
+  ADD PRIMARY KEY (`customerID`);
 
 --
 -- Indexes for table `manufacturer`
 --
 ALTER TABLE `manufacturer`
-  ADD PRIMARY KEY (`manufacturer_id`);
+  ADD PRIMARY KEY (`manufacturerID`);
 
 --
--- Indexes for table `order`
+-- Indexes for table `orders`
 --
-ALTER TABLE `order`
-  ADD PRIMARY KEY (`order_id`);
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`order_id`),
+  ADD KEY `fk_customer` (`customer_id`),
+  ADD KEY `fk_product` (`product_id`);
 
 --
 -- Indexes for table `product`
 --
 ALTER TABLE `product`
-  ADD PRIMARY KEY (`product_id`);
+  ADD PRIMARY KEY (`productID`),
+  ADD KEY `fk_category` (`categoryid`),
+  ADD KEY `fk_manufacturer` (`manufacturerid`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `category`
+--
+ALTER TABLE `category`
+  MODIFY `categoryID` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT for table `customer`
+--
+ALTER TABLE `customer`
+  MODIFY `customerID` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+
+--
+-- AUTO_INCREMENT for table `manufacturer`
+--
+ALTER TABLE `manufacturer`
+  MODIFY `manufacturerID` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `order_id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `product`
+--
+ALTER TABLE `product`
+  MODIFY `productID` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `orders`
+--
+ALTER TABLE `orders`
+  ADD CONSTRAINT `fk_customer` FOREIGN KEY (`customer_id`) REFERENCES `customer` (`customerID`),
+  ADD CONSTRAINT `fk_product` FOREIGN KEY (`product_id`) REFERENCES `product` (`productID`);
+
+--
+-- Constraints for table `product`
+--
+ALTER TABLE `product`
+  ADD CONSTRAINT `fk_category` FOREIGN KEY (`categoryid`) REFERENCES `category` (`categoryID`),
+  ADD CONSTRAINT `fk_manufacturer` FOREIGN KEY (`manufacturerid`) REFERENCES `manufacturer` (`manufacturerID`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
