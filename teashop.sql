d3c6b29b07f74e0a59e69e25290550f7cb9a5c4b
Chase Dallmann <chasedallmann@comcast.net>
2025-03-26 19:11:39 -0500

    Add files via upload
    Signed-off-by: Chase Dallmann <chasedallmann@comcast.net>

--- teashop.sql
+++ teashop.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Mar 19, 2025 at 04:05 PM
+-- Generation Time: Mar 27, 2025 at 12:10 AM
 -- Server version: 8.0.39
 -- PHP Version: 8.2.23
 
@@ -44,6 +44,40 @@ INSERT INTO `category` (`categoryID`, `categoryName`, `categoryDescription`) VAL
 (4, 'Oolong Tea', 'Oolong is similar to green tea with a lighter oxidation process. Potential health benefits include reduced chronic diseases, increased metabolism, and antioxidants.'),
 (5, 'Herbal Tea', 'Herbal tea is made of herbs, fruit, and spices. There are various health benefits of herbal tea that are dependent on the ingredients used.'),
 (6, 'Pu-erh Tea', 'Pu-erh tea comes from the Yunnan province in China and has a rich, earthy flavor. Potential health benefits include an increased metabolism, anti-cancer properties, and boosted liver function.');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `chatTranscript`
+--
+
+CREATE TABLE `chatTranscript` (
+  `chatTranscriptID` int NOT NULL,
+  `sender` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `receiver` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `message` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `status` enum('JOIN','MESSAGE','LEAVE') CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `timestamp` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `chatTranscript`
+--
+
+INSERT INTO `chatTranscript` (`chatTranscriptID`, `sender`, `receiver`, `message`, `status`, `timestamp`) VALUES
+(1, 'Chase Dallmann', 'customerService', 'Hello', 'MESSAGE', '2025-03-26 07:47:01'),
+(2, 'Chase Dallmann', 'Chase Dallmann', 'test', 'MESSAGE', '2025-03-26 07:49:09'),
+(3, 'Chase Dallmann', 'Chase Dallmann', 'Now', 'MESSAGE', '2025-03-26 07:49:39'),
+(4, 'Chase Dallmann', 'Chase Dallmann', 'testing', 'MESSAGE', '2025-03-26 07:50:53'),
+(5, 'Chase Dallmann', 'customer', 'Hello', 'MESSAGE', '2025-03-26 07:54:44'),
+(6, 'Chase Dallmann', 'customer', 'How are you?', 'MESSAGE', '2025-03-26 07:54:50'),
+(7, 'John Doe', 'John Doe', 'I need assistance', 'MESSAGE', '2025-03-26 07:55:47'),
+(8, 'Chase Dallmann', 'customer', 'Okay', 'MESSAGE', '2025-03-26 07:55:54'),
+(9, 'Chase Dallmann', 'customer', 'Hello there', 'MESSAGE', '2025-03-26 13:09:15'),
+(10, 'Chase Dallmann', 'customer', 'Hello', 'MESSAGE', '2025-03-26 13:20:22'),
+(11, 'Trial User', 'Trial User', 'Hello', 'MESSAGE', '2025-03-26 19:05:29'),
+(12, 'Trial User', 'Trial User', 'I want to buy some tea', 'MESSAGE', '2025-03-26 19:05:37'),
+(13, 'Trial User', 'Trial User', 'Hello', 'MESSAGE', '2025-03-26 19:08:02');
 
 -- --------------------------------------------------------
 
@@ -91,6 +125,7 @@ CREATE TABLE `product` (
   `productName` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `productDescription` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
   `price` float(5,2) NOT NULL,
+  `imageURL` varchar(250) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
   `productInventory` int NOT NULL,
   `categoryid` int NOT NULL,
   `manufacturerid` int NOT NULL
@@ -100,16 +135,16 @@ CREATE TABLE `product` (
 -- Dumping data for table `product`
 --
 
-INSERT INTO `product` (`productID`, `productName`, `productDescription`, `price`, `productInventory`, `categoryid`, `manufacturerid`) VALUES
-(1, 'Boba Tea', 'A delicious boba tea', 10.99, 10, 2, 1),
-(2, 'Organic Turmeric', 'Loose Leaf 2 OZ', 11.99, 10, 2, 1),
-(3, 'Organic Earl Grey', 'Loose Leaf 2 OZ', 14.99, 10, 1, 1),
-(4, 'Vanilla', 'Loose Leaf 2 OZ', 12.99, 10, 1, 1),
-(5, 'Organic Oolong', 'Loose Leaf 2 OZ', 16.99, 10, 4, 1),
-(6, 'Organic Royal Oolong', 'Loose Leaf 2 OZ', 14.99, 10, 4, 1),
-(7, 'Mutan White', 'Loose Leaf 1.5 OZ ', 11.00, 10, 3, 3),
-(8, 'Chamomile Tea Bags', '8 OZ', 7.99, 10, 5, 2),
-(9, 'Pu-erh Loose Leaf', '8 OZ', 12.99, 10, 6, 2);
+INSERT INTO `product` (`productID`, `productName`, `productDescription`, `price`, `imageURL`, `productInventory`, `categoryid`, `manufacturerid`) VALUES
+(1, 'Boba Tea', 'A delicious boba tea', 10.99, 'https://insanelygoodrecipes.com/wp-content/uploads/2023/05/Refreshing-Boba-Milk-Tea-with-Pearls.jpg', 10, 2, 1),
+(2, 'Organic Turmeric', 'Loose Leaf 2 OZ', 11.99, 'https://static.trotcdn.com/images/1000/V20245_2.jpg', 10, 2, 1),
+(3, 'Organic Earl Grey', 'Loose Leaf 2 OZ', 14.99, 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.YLWIAJb-2IC3PxfUznIzJgHaE7%26pid%3DApi&f=1&ipt=8d60c862465f0b505fb6d48da32d340311efc806c03be9a8633c9cc750a806d5&ipo=images', 10, 1, 1),
+(4, 'Vanilla', 'Loose Leaf 2 OZ', 12.99, 'https://www.kroger.com/product/images/large/back/0007231000165', 10, 1, 1),
+(5, 'Organic Oolong', 'Loose Leaf 2 OZ', 16.99, 'https://n4.sdlcdn.com/imgs/h/2/3/Teafloor-Organic-Oolong-Tea-Loose-SDL775610821-2-2f99c.jpg', 10, 4, 1),
+(6, 'Organic Royal Oolong', 'Loose Leaf 2 OZ', 14.99, 'https://www.organicfacts.net/wp-content/uploads/oolongtea-1.jpg', 10, 4, 1),
+(7, 'Mutan White', 'Loose Leaf 1.5 OZ ', 11.00, 'https://uptownteashop.com/cdn/shop/files/uptown-tea-shop-fb-organic-mutan-white-tea_1500x.jpg?v=1710343364', 10, 3, 3),
+(8, 'Chamomile Tea Bags', '8 OZ', 7.99, 'https://www.bestofhungary.co.uk/cdn/shop/files/ChamomileTeaBags.jpg?crop=center&height=1500&v=1716715128&width=1500', 10, 5, 2),
+(9, 'Pu-erh Loose Leaf', '8 OZ', 12.99, 'https://cdn.shopify.com/s/files/1/0164/3912/products/pu-erh_loose_copy_grande.jpg?v=1405907665', 10, 6, 2);
 
 -- --------------------------------------------------------
 
@@ -141,9 +176,13 @@ INSERT INTO `user` (`userID`, `role`, `firstName`, `lastName`, `email`, `streetA
 (15, 'CUSTOMER', 'NZMEM', 'HZXMY', 'ZN289@yahoo.com', '496 LWJP', 'NNYSF', 'RUP', 67386, 'IFOVZ038'),
 (17, 'CUSTOMER', 'UPLSN', 'LTRGA', 'ZQ929@hotmail.com', '256 DNIQ', 'GFHGB', 'RCB', 70480, 'DMWBP584'),
 (19, 'CUSTOMER', 'IOGKY', 'XEIDG', 'WX881@aol.com', '511 EKNW', 'EHCFY', 'SXO', 90761, 'TEWKS239'),
-(27, 'CUSTOMER_SERVICE', 'Chase', 'Dallmann', 'dallmanc@csp.edu', '11720 Vermillion St Ne Unit D ', 'Blaine', 'MN', 55449, '$2a$10$jgA2/T/iCH3kornkHbOzLu0kRPKDmFLlZSazE5jxBDNkbxKVs3rnK'),
+(27, 'CUSTOMER_SERVICE', 'Chase', 'Test', 'test123@csp.edu', '', '', 'MN', 55449, '$2a$10$jgA2/T/iCH3kornkHbOzLu0kRPKDmFLlZSazE5jxBDNkbxKVs3rnK'),
 (28, 'CUSTOMER', 'Chase', 'Dallmann', 'testuser@csp.edu', '11720 Vermillion St Ne Unit D', 'Blaine', 'MN', 55449, '$2a$10$BdPlZ4fgbbHu3TtdinBF0eL/JZ4VojF77aymvdoYU.jwVOarg0kfi'),
-(29, 'CUSTOMER_SERVICE', 'Test', 'User', 'testuser@yahoo.com', '1234 Fake St ', 'Blaine', 'MN', 55449, '$2a$10$cE/yLg6XNmvSVHSiknyVX.7iql3sNtcsfY0ZhraTH/7/2brVnLAGW');
+(29, 'CUSTOMER_SERVICE', 'Test', 'User', 'testuser@yahoo.com', '1234 Fake St ', 'Blaine', 'MN', 55449, '$2a$10$cE/yLg6XNmvSVHSiknyVX.7iql3sNtcsfY0ZhraTH/7/2brVnLAGW'),
+(30, 'CUSTOMER', 'John', 'Doe', 'trial@aceteas.com', '1234 Fake St Unit D', 'MINNEAPOLIS', 'MN', 55112, '$2a$10$Woaw7TYtdUzXrSbtVvwEC.Ay0gISsohakq3RloxZx6lmBbkfADpqO'),
+(31, 'CUSTOMER', 'John', 'Doe', 'fake@csp.edu', '', '', 'MN', 55449, '$2a$10$EeMQ6XFfNIxBMa28mQf8xeVx4v.yFREo7fpRQvkvtWycFmz1n/GwW'),
+(32, 'CUSTOMER', 'Chase', 'Dallmann', 'dallmanc@csp.edu', '11720 Vermillion St Ne Unit D ', 'Blaine', 'MN', 55449, '$2a$10$P9mtz9wZeGmpN8OnrDLacuibKPZJIqEjBKlC4htg7AwmsXaLVx7Ru'),
+(33, 'CUSTOMER', 'Chase', 'Test', 'trial@test.com', '1234 Fake St Unit D', 'MINNEAPOLIS', 'MN', 55113, '$2a$10$BoU.vRlFGmk2oXvCkKHHpu2mIPJbP3VJuMNLkjuoJ1UK9OZd9sdMS');
 
 --
 -- Indexes for dumped tables
@@ -154,6 +193,12 @@ INSERT INTO `user` (`userID`, `role`, `firstName`, `lastName`, `email`, `streetA
 --
 ALTER TABLE `category`
   ADD PRIMARY KEY (`categoryID`);
+
+--
+-- Indexes for table `chatTranscript`
+--
+ALTER TABLE `chatTranscript`
+  ADD PRIMARY KEY (`chatTranscriptID`);
 
 --
 -- Indexes for table `manufacturer`
@@ -194,6 +239,12 @@ ALTER TABLE `category`
   MODIFY `categoryID` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
+-- AUTO_INCREMENT for table `chatTranscript`
+--
+ALTER TABLE `chatTranscript`
+  MODIFY `chatTranscriptID` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
 -- AUTO_INCREMENT for table `manufacturer`
 --
 ALTER TABLE `manufacturer`
@@ -215,7 +266,7 @@ ALTER TABLE `product`
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `userID` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
+  MODIFY `userID` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
 
 --
 -- Constraints for dumped tables
